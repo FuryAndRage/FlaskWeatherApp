f0724aa02ac0763939e595249ca1c47e8ef9c46a
Moises <lawliex@gmail.com>
2020-07-14 00:35:39 +1200

    .

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, request
+import requests
+from secret import secret_api_key
+
+app = Flask(__name__)
+
+
+def get_api():
+	city = 'Auckland'
+	api_key = secret_api_key()
+	url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+	req = requests.get(url)
+	res = []
+	for item in req.json().values():
+		res.append(item)
+	print(res[1][0]['icon'])
+	return res
+
+	# print({chave:valor for (chave, valor) in req_json.items()})
+
+@app.route('/')
+def home():
+	
+	return render_template('index.html', res = get_api())
+
+if __name__ == '__main__':
+	app.run()
